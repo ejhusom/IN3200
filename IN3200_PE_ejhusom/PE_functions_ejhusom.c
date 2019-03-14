8d1f96c9a6353ff6681cf722bd1dcff9e2e2999b
ejhusom <ejlgkvam@online.no>
2019-03-14 11:49:50 +0000

    initial commit of partial exam

---
+++ IN3200_PE_ejhusom/PE_functions_ejhusom.c
@@ -0,0 +1,13 @@
+#include <PE_functions_ejhusom.h>
+
+read_graph_from_file(){
+
+}
+
+PageRank_iterations(){
+
+}
+
+top_n_webpages(){
+
+}
---
+++ IN3200_PE_ejhusom/PE_functions_ejhusom.h
@@ -0,0 +1,8 @@
+#ifndef PE_FUNCTIONS_EJHUSOM_H
+#define PE_FUNCTIONS_EJHUSOM_H
+
+read_graph_from_file();
+PageRank_iterations();
+top_n_webpages();
+
+#endif /* PE_FUNCTIONS_EJHUSOM_H */
