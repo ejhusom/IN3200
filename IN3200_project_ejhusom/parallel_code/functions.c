b6e710ecc432c9bbaa392c6e3e16bd9791cd5e0b
ejhusom <ejlgkvam@online.no>
2019-05-08 15:27:06 +0200

    dividing image works!

---
+++ IN3200_project_ejhusom/parallel_code/functions.c
@@ -0,0 +1,68 @@
+#include "functions.h"
+
+void allocate_image(image *u, int m, int n){
+    u->m = m;
+    u->n = n;
+    u->image_data = (float**) malloc(m * sizeof(float*));
+
+    for (int i = 0; i < m; i++) {
+        u->image_data[i] = (float*)malloc(n * sizeof(float));
+    }
+
+}
+
+void deallocate_image(image *u){
+    for (int i = 0; i < u->m; i++){
+        free(u->image_data[i]);
+    } 
+    free(u->image_data);
+}
+
+void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
+    for (int i = 0; i < u->m; i++){
+        for (int j = 0; j < u->n; j++){
+            u->image_data[i][j] = (float)image_chars[u->n*i + j];
+        }
+    }
+}
+
+void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
+    for (int i = 0; i < u->m; i++){
+        for (int j = 0; j < u->n; j++){
+            image_chars[u->n*i + j] = u->image_data[i][j];
+        }
+    }
+}
+
+
+void iso_diffusion_denoising_parallel(image *u, image *u_bar, float kappa, int iters, int num_procs){
+    for (int i = 0; i < u->m; i++){
+        u_bar->image_data[i][0] = u->image_data[i][0];
+        u_bar->image_data[i][u->n-1] = u->image_data[i][u->n-1];
+    }
+    if (my_rank==0 || my_rank==(num_procs -1)){
+        for (int j = 1; j < u->n-1; j++){
+            u_bar->image_data[0][j] = u->image_data[0][j];
+            u_bar->image_data[u->m-1][j] = u->image_data[u->m-1][j];
+        }
+    }
+
+    for (int it = 0; it < iters; it++){
+        for (int i = 1; i < u->m-1; i++){
+            for (int j = 1; j < u->n-1; j++){
+                u_bar->image_data[i][j] = u->image_data[i][j] 
+                                            + kappa*(u->image_data[i-1][j] 
+                                                    + u->image_data[i][j-1] 
+                                                    - 4*u->image_data[i][j] 
+                                                    + u->image_data[i][j+1] 
+                                                    + u->image_data[i+1][j]); 
+            }
+        } 
+        for (int i = 1; i < u->m-1; i++){
+            for (int j = 1; j < u->n-1; j++){
+                u->image_data[i][j] = u_bar->image_data[i][j];
+            }
+        }
+    }
+    
+}
--- IN3200_project_ejhusom/parallel_code/parallel_main.c
+++ IN3200_project_ejhusom/parallel_code/parallel_main.c
@@ -1,5 +1,4 @@
-#include "../serial_code/functions.h"
-#include "parallel_function.h"
+#include "functions.h"
 
 void import_JPEG_file (const char* filename, unsigned char** image_chars,
                        int* image_height, int* image_width,
@@ -13,13 +12,14 @@ int main (int argc, char *argv[])
 {
 
     int m, n, c; 
-    int my_m, my_n, my_rank, num_procs;
+    int my_m, my_start, my_stop, my_rank, num_procs;
     int iters = 100;
     float kappa = 0.2;
     image u, u_bar, whole_image;
     unsigned char *image_chars, *my_image_chars;
     char *input_jpeg_filename;
     char *output_jpeg_filename = "denoised.jpeg";
+    MPI_Comm comm;
 
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
@@ -45,41 +45,74 @@ int main (int argc, char *argv[])
             return 0;
     }
  
+    /* IMPORTING IMAGE */
     if (my_rank==0){
         import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
-        allocate(&whole_image, m, n);
+        allocate_image(&whole_image, m, n);
     }
 
-    MPI_Bcast(&m, 1, MPI_INT, 0, MPIP_COMM_WORLD);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPIP_COMM_WORLD);
+
+
+    /* FINDING REGION AND BOUNDS FOR EACH PROCESS */
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    int m_partition = m/num_procs;
+    int m_rest = m%num_procs;
+    if (m_rest > 0){
+        my_start = n*my_rank*(m_partition + 1);
+        if (my_rank < num_procs-1){
+            my_m = m_partition + 1;
+            my_stop = n*(my_rank + 1)*(m_partition + 1);
+        } else {
+            my_stop = m*n;
+            my_m = m - my_start/n;
+        }
+    } else {
+        my_start = n*my_rank*m_partition;
+        my_m = m_partition;
+        my_stop = n*(my_rank + 1)*m_partition;
+    }
+
+    printf("Rank=%d, my_m=%d, start=%d, stop=%d\n", my_rank, my_m, my_start, my_stop);
+
+    int *sizes, *start_idx;
+    int my_size = my_m*n;
+    if (my_rank == 0){
+        sizes = (int*)malloc(num_procs*sizeof(int));
+        start_idx = (int*)malloc(num_procs*sizeof(int));
+    }
+    MPI_Gather(&my_size, 1, MPI_INT, sizes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(&my_start, 1, MPI_INT, start_idx, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    my_image_chars = (unsigned char*)malloc(my_size*sizeof(unsigned char));
+
+    MPI_Scatterv(image_chars, sizes, start_idx, MPI_UNSIGNED_CHAR, my_image_chars, my_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+
+    allocate_image(&u, my_m, n);
+    allocate_image(&u_bar, my_m, n);
+    convert_jpeg_to_image(my_image_chars, &u);
     
-    m_overflow = m%num_procs;
-    n_overflow = n%num_procs;
-    my_m = ;
-    my_n = 0;
 
-    allocate_image(&u, m, n);
-    allocate_image(&u_bar, m, n);
+    /* CREATING COMMUNICATOR */
+    int dim[2], period[2], reorder, dim_count;
+    dim[0] = 0; dim[1] = 1;
+    period[0] = 0; period[1] = 0;
+    reorder = 0;
+    dim_count = 2;
+    int my_cart_coords[2];
+    MPI_Dims_create(num_procs, dim_count, dim);
+    MPI_Cart_create(MPI_COMM_WORLD, dim_count, dim, period, reorder, &comm);
+    MPI_Cart_coords(comm, my_rank, dim_count, my_cart_coords);
 
-    /* each process asks process 0 for a partitioned region */
-    /* of image_chars and copy the values into u */
-    /*  ...  */
 
-    convert_jpeg_to_image(image_chars, &u);
-    
     /* DENOISING IMAGE AND MEASURING TIME USAGE */
     struct timespec start, end;
     clock_gettime(CLOCK_REALTIME, &start);
-    iso_diffusion_denoising_parallel(&u, &u_bar, kappa, iters);
-
-    /* each process sends its resulting content of u_bar to process 0 */
-    /* process 0 receives from each process incoming values and */
-    /* copy them into the designated region of struct whole_image */
-    /*  ...  */
-
+    iso_diffusion_denoising_parallel(&u, &u_bar, kappa, iters, num_procs);
     clock_gettime(CLOCK_REALTIME, &end);
     double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-
+    printf("Time used: %f\n", time_spent);
     /* WRITING TIME MEASUREMENT TO FILE */
 //    FILE *outfile = fopen("timing.txt", "a");
 //    fprintf(outfile, "%f\n", time_spent);
@@ -87,14 +120,39 @@ int main (int argc, char *argv[])
 
     printf("Image is denoised!\n");
 
+    convert_image_to_jpeg(&u, my_image_chars);
+
+    /* PRINT SUBIMAGE FOR EACH PROCESS */
+//    if (my_rank==0){
+//    char *name = "test0.jpeg";
+//    export_JPEG_file(name, my_image_chars, my_m, n, 1, 75);
+//    }
+//    if (my_rank==1){
+//    char *name = "test1.jpeg";
+//    export_JPEG_file(name, my_image_chars, my_m, n, 1, 75);
+//    }
+//    if (my_rank==2){
+//    char *name = "test2.jpeg";
+//    export_JPEG_file(name, my_image_chars, my_m, n, 1, 75);
+//    }
+//    if (my_rank==3){
+//    char *name = "test3.jpeg";
+//    export_JPEG_file(name, my_image_chars, my_m, n, 1, 75);
+//    }
+
+    MPI_Gatherv(my_image_chars, my_size, MPI_UNSIGNED_CHAR, image_chars, sizes, start_idx, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
     if (my_rank==0){
-        convert_image_to_jpeg(&whole_image, image_chars);    
         export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
         deallocate_image(&whole_image);
+        free(image_chars);
     }
 
     deallocate_image(&u);
     deallocate_image(&u_bar);
+    free(my_image_chars);
+    free(sizes);
+    free(start_idx);
 
     MPI_Finalize();
     return 0;
