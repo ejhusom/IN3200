509a2d9a5709a9d065be7ee01d12355aa99bc298
ejhusom <ejlgkvam@online.no>
2019-05-09 14:58:57 +0200

    fixed timing

--- IN3200_project_ejhusom/timing.py
+++ IN3200_project_ejhusom/timing.py
@@ -18,24 +18,26 @@ plt.figure(figsize=(w,h))
 
 num_procs = np.array([2, 3, 4, 5, 6])
 parallel_timings = np.zeros(5)
-
 parallel_data = np.loadtxt('parallel_code/timing_parallel_4_procs.txt')
-
 
 for i in num_procs:
     parallel_data = np.loadtxt('parallel_code/timing_parallel_{:d}_procs.txt'.format(i))
-    timings = parallel_data[:]
-    mean_time = np.mean(timings)
-    parallel_timings[i] = mean_time
+    mean_time = np.mean(parallel_data)
+    parallel_timings[(i-2)] = mean_time
 
 
-plt.plot(num_procs, parallel_timings)
+plt.plot(num_procs, parallel_timings, '.-', label='parallel code')
 
 plt.xlabel("number of threads")
 plt.ylabel("time usage [s]")
 plt.xticks([2,3,4,5,6])
-plt.legend()
 plt.tight_layout()
 plt.savefig("runtime.pdf",dpi=300)
 plt.show()
 
+
+serial_data = np.loadtxt('serial_code/timing_serial.txt')
+serial_timings = np.mean(serial_data)
+
+print("Serial mean time: {:f}".format(serial_timings))
+print("Parallel minimum time: {:f}".format(np.min(parallel_timings)))
